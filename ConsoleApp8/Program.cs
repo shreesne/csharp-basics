0466e8d79573444023108a07d53a32081abe8f38
SHREE\sneha <snehsswagh25@gmail.com>
2023-09-02 15:42:00 +0200

    push

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+   
+
+class Program
+        {
+            static void Main()
+            {
+                List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Define a Predicate that checks if a number is even
+            Predicate<int> isEven = x => x % 2 == 0;
+
+                // Use FindAll to filter the list based on the Predicate
+                List<int> evenNumbers = numbers.FindAll(isEven);
+
+                // Display the even numbers
+                foreach (int number in evenNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }
+
+
