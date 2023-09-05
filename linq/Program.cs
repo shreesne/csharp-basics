9d357db34d6e4a66870a8b63b1e0f30a173930d0
SHREE\sneha <snehsswagh25@gmail.com>
2023-09-05 13:48:46 +0200

    linq example

---
+++ linq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           
+                // Data source
+                string[] names = { "Bill", "Steve", "James", "Mohan" };
+
+                // LINQ Query 
+                var myLinqQuery = from name in names
+                                  where name.Contains('a')
+                                  select name;
+
+                // Query execution
+                foreach (var name in myLinqQuery)
+                     Console.Write(name + " ");
+                   Console.ReadKey();
+            }
+        }
+    }
+    
