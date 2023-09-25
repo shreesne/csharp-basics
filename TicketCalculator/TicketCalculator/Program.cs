e408d416160020da633e9bf285c1715c5c5cc287
SHREE\sneha <snehsswagh25@gmail.com>
2023-09-25 14:49:19 +0200

    final output

---
+++ TicketCalculator/TicketCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TicketCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+           Console.Write("Enter your age: ");
+            int age=int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if(age>=0)
+            {
+                Console.Write("Your prefernce to seat[yes/no]: ");
+                string place = Console.ReadLine();
+                Console.WriteLine();
+                if (place=="yes"||place=="no")
+                {
+                    TicketCalculator obj = new TicketCalculator();
+                    var price = obj.PriceSetter(age, place);
+                    Console.WriteLine($"Ticket price before tax:{price} SEK(kronor)");
+                    Console.WriteLine();
+                    var priceAfterTax = obj.TaxCalculator(price);
+                    Console.WriteLine($"Ticket price after tax:{priceAfterTax} SEK(kronor)");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid place input");
+                }
+            }
+          else { Console.WriteLine("Invalid age input"); }
+
+            TicketCalculator obj1 = new TicketCalculator();
+            int ticketNo= obj1.TicketNumberGenerator();
+            Console.WriteLine("Your ticket number is :" + ticketNo);
+        }
+    }
+}
---
+++ TicketCalculator/TicketCalculator/TicketCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketCalculator
+{
+
+    public class TicketCalculator
+    {
+        public int PriceSetter(int age, string place)
+        {
+            int price = 0;
+            bool isSeating = place.Equals("yes", StringComparison.OrdinalIgnoreCase);
+            if (age <= 11)
+            {
+
+                if (isSeating)
+                {
+                    return price = 50;
+                }
+                else
+                {
+                    return price = 25;
+                }
+            }
+            else if (age >= 12 && age <= 64)
+            {
+                if (isSeating)
+                {
+                    return price = 170;
+                }
+                else
+                {
+                    return price = 110;
+                }
+            }
+            else if (age >= 65)
+            {
+                if (isSeating)
+                {
+                    return price = 100;
+                }
+                else
+                {
+                    return price = 60;
+                }
+            }
+            return price;
+        }
+
+       
+
+        public decimal TaxCalculator (int Price) 
+        {
+            decimal tax;
+            tax = Price + (Price * 6 / 100);
+            return tax;
+        }
+
+        public int TicketNumberGenerator()
+        {
+            Random rnd = new Random();
+            int minVal = 1;
+            int maxVal = 8000;
+           return rnd.Next(minVal,maxVal+1);
+        }
+    }
+}
