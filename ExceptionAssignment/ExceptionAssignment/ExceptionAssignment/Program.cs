c4f28958fc04a3599e57abe580bdecc0994c4408
SHREE\sneha <snehsswagh25@gmail.com>
2023-10-11 13:58:16 +0200

    added 2 methods

---
+++ ExceptionAssignment/ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExceptionAssignment
+{
+ public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string of numbers separated by spaces:");
+            string input = Console.ReadLine();
+
+            List<int> sortedNumbers =WordsDTO.ExtractAndSortNumbers(input);
+
+            Console.WriteLine("Sorted numbers (highest to lowest):");
+            foreach (int number in sortedNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("enter a file path with seperated comma :");
+            string filePath = Console.ReadLine();
+            WordsDTO result = WordsDTO.ReadFileAndCreateDTO(filePath);
+
+            Console.WriteLine("BooleanValue: " + result.BooleanValue);
+            Console.WriteLine("StringValue: " + result.StringValue);
+
+            if (result.StringArray != null)
+            {
+                Console.WriteLine("StringArray:");
+                foreach (string word in result.StringArray)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
---
+++ ExceptionAssignment/ExceptionAssignment/ExceptionAssignment/WordsDTO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionAssignment
+{
+    public class WordsDTO
+    {
+        public string[] StringArray { get; set; }
+        public bool BooleanValue { get; set; }
+        public string StringValue { get; set; }
+
+       public static List<int> ExtractAndSortNumbers(string input)
+        {
+            List<int> numbers = new List<int>();
+            string[] numberStrings=input.Split(' ');
+ 
+            foreach (string numString in numberStrings)
+            {
+                try
+                {
+                    int number = int.Parse(numString);
+                    numbers.Add(number);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Entry discarded");
+                }
+            }
+
+            numbers.Sort((a, b) => b.CompareTo(a));
+            return numbers;
+        }
+        public static WordsDTO ReadFileAndCreateDTO(string filePath)
+        {
+          WordsDTO dto = new WordsDTO();
+
+            try
+            {
+                if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+                string fileContent = File.ReadAllText(filePath);
+                string[] words = fileContent.Split(',');
+                dto.StringArray = words;
+                dto.BooleanValue = true;
+                dto.StringValue = "Success";
+
+            }
+            catch(Exception ex)
+            {
+                dto.BooleanValue = false;
+                dto.StringValue = "File Not Found";
+            }
+
+            return dto;
+        }
+    }
+
+}
